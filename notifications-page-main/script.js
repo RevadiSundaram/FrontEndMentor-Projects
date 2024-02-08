f43c0d9a4ddad721437a2d9c530623ed63271e27
revadisundaram7 <sundaramrevadi@gmail.com>
2024-02-08 20:57:35 +0530

    Modified js

--- notifications-page-main/script.js
+++ notifications-page-main/script.js
@@ -1,27 +1,32 @@
 const numOfUnreadMsg = document.getElementById('numMsg');
-let count = 3;
-const markAsRead = document.getElementById('markRead');
+const markAsRead = document.querySelector('.markRead');
 const notificationMsg = document.querySelectorAll('.card');
 const cardComponent  =document.querySelector('.card');
 const redIcon = document.querySelectorAll('.icon');
+const iconEl = document.querySelector('.icon');
 
 markAsRead.addEventListener('click' , function(){
-    numOfUnreadMsg.textContent = 0;
-    for(let i=0; i<notificationMsg.length; i++){
-        notificationMsg[i].style.backgroundColor = 'hsl(0, 0%, 100%)';
-        redIcon[i].remove();
-    }
-    // for(let i=0; redIcon.length; i++){
-        
-    // }
-});
+    notificationMsg.forEach(function (notification,index){
+        notification.classList.remove('unread');
+        iconEl.classList.toggle('null');
+            redIcon[index].remove();
+            unreadCount();   
+           
+    } )   
+    
+    });
+
 
 notificationMsg.forEach(function (msg, index){
     msg.addEventListener('click',function(){
-        msg.style.backgroundColor = 'hsl(0, 0%, 100%)';
+        msg.classList.toggle('unread');
+        // iconEl.classList.add('null');
         redIcon[index].remove();
-        count--;
-        numOfUnreadMsg.textContent = count;
-        
+        unreadCount();
     })
 })
+function unreadCount(){
+    const unread = document.querySelectorAll('.card.unread').length;
+    numOfUnreadMsg.textContent = unread;
+    console.log(unread);
+}
