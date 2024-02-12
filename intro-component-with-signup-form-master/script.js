9b5ab6e80ac943dcc7a0a6e1af681309c39d4a2b
revadisundaram7 <sundaramrevadi@gmail.com>
2024-02-12 19:15:54 +0530

    Adding js

--- intro-component-with-signup-form-master/script.js
+++ intro-component-with-signup-form-master/script.js
@@ -3,14 +3,44 @@ const lastName = document.getElementById("lastName");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 const submitBtn = document.getElementById("submit");
+const form = document.getElementById("signup");
 
-console.log(lastName);
+const errorElement = document.getElementsByClassName('span');
 
 
-submitBtn.addEventListener('click',function(){
-    console.log("firstName");
-    if(firstName == ''){
-        console.log('first name should not be empty');
-        alert('first name should not be empty');
+submitBtn.addEventListener('click', function(e){
+    e.preventDefault();
+    if(firstName.value == ''){
+        // console.log(firstName || "first");
+            firstName.classList.add('border');
+            errorElement[0].textContent = "First Name cannot be empty";
+            // console.log(errorElement.textContent);
+       }
+    if(lastName.value == ''){
+        // lastName.textContent += `<svg width="24" height="24" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><circle fill="#FF7979" cx="12" cy="12" r="12"/><rect fill="#FFF" x="11" y="6" width="2" height="9" rx="1"/><rect fill="#FFF" x="11" y="17" width="2" height="2" rx="1"/></g></svg>`;
+        lastName.classList.add('border');
+        errorElement[1].textContent = "Last Name cannot be empty";
     }
-});
+    if(password.value == ''){
+        // lastName.textContent += `<svg width="24" height="24" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><circle fill="#FF7979" cx="12" cy="12" r="12"/><rect fill="#FFF" x="11" y="6" width="2" height="9" rx="1"/><rect fill="#FFF" x="11" y="17" width="2" height="2" rx="1"/></g></svg>`;
+        password.classList.add('border');
+        errorElement[3].textContent = "Password cannot be empty";
+    }
+    console.log(validateEmail(email.value));
+    if(email.value != validateEmail(email.value)){
+        email.value = '';
+        email.value = 'email@email.com';
+        email.style.color = 'hsl(0, 100%, 74%)';
+        email.classList.add('border');
+        errorElement[2].textContent = "Looks like this is not an email";
+    }
+    
+})
+function validateEmail(email){
+    let regEx = /\S+@\S+\.\S+/;
+    return regEx.test(email);
+}
+
+  
+    
+
